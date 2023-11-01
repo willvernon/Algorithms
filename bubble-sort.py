12e903913ff9f5d270bf7fa83c279410ee1bc315
willvernon <wmvernon@proton.me>
2023-11-01 12:50:14 -0400

    add: bubble and binary search

---
+++ bubble-sort.py
@@ -0,0 +1,11 @@
+
+arr = [1,9,8,4,3,6]
+length = len(arr)
+
+for i in range(length):
+    for j in range(length -1 -i):
+        if(arr[j] > arr[j + 1]):
+            tmp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j + 1] = tmp
+            print(arr)           
