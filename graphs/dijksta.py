a2c0238941fca4a63cb3b6c1b3224e5b1ddee6db
willvernon <wmvernon@proton.me>
2023-12-26 17:54:59 -0500

    dijksta

---
+++ graphs/dijksta.py
@@ -0,0 +1,20 @@
+function Dijkstra(Graph, source):
+    dist[source] = 0
+    create vertex set Q
+
+    for each vertex v in Graph:
+        if v != source:
+            dist[v] = INFINITY
+            prev[v] = UNDEFINED
+        Q.add_with_priority(v, dist[v])
+
+    while Q is not empty:
+        u = Q.extract_min()
+        for each neighbor v of u:
+            alt = dist[u] + length(u, v)
+            if alt < dist[v]:
+                dist[v] = alt
+                prev[v] = u 
+                Q.descrease_priority(v, alt)
+
+    return dist[], prev[]
